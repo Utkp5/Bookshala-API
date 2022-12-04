04c00dac7ddc6c3d2f193ae8e0efbc4192b55b85
Utkp5 <utkp200@gmail.com>
2022-12-04 21:36:32 +0530

    bookshala

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+
+
+app.get("/" , (req,res) => {
+    return res.send("ok ji dn")
+})
+
+const start = async () => {
+    try {
+        
+        app.listen(PORT , () =>  {
+            console.log(`Server started Successfully on PORT : ${PORT}`);
+        })
+
+    } catch (error) {
+        console.log("error")
+    }
+}
+
+start();
